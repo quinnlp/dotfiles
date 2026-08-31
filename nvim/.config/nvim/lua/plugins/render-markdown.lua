73e56259d783ca80bf4cfe5509ba4150905beaf4
Quinn Pham <quinnlp12@gmail.com>
2026-08-31 20:33:24 +0000

    markdown

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim#lazynvim
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+	    'nvim-treesitter/nvim-treesitter',
+	    'nvim-tree/nvim-web-devicons'
+    },
+    opts = {},
+}
